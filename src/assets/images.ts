5a0a465ce3bd3325b5d922b1c773a391addb1a65
Tim Günther <tim.titus.guenther@mni.thm.de>
2026-02-11 17:38:25 +0100

    optimized image storage and rendering

---
+++ src/assets/images.ts
@@ -0,0 +1,26 @@
+import rudernEiner from "./images/rudernEiner.jpeg";
+import drachenbootNah from "./images/drachenbootNah.jpeg";
+import rudernBreitSport from "./images/rudernBreitSport.jpeg";
+import rudernDoppelzweier from "./images/rudernDoppelzweier.jpg";
+import rudernKind from "./images/rudernKind.jpeg";
+import rudernPodium from "./images/rudernPodium.jpeg";
+import drachenbootAction from "./images/drachenbootAction.png";
+import drachenbootHinten from "./images/drachenbootHinten.jpeg";
+import hallensport from "./images/hallensport.png";
+import sportabzeichen from "./images/sportabzeichen.jpg";
+import logo from "./images/220926_HellasGiessen-Logo-Web.png";
+
+export {
+  rudernEiner,
+  drachenbootNah,
+  rudernBreitSport,
+  rudernDoppelzweier,
+  rudernKind,
+  rudernPodium,
+  drachenbootAction,
+  drachenbootHinten,
+  hallensport,
+  sportabzeichen,
+  logo,
+};
+
